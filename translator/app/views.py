0c09d1bc7acaf36ec2482b121272029bc6b1178e
Jude Oyedele <judeokennywise@gmail.com>
2023-04-26 14:07:40 +0100

    initial commit

---
+++ translator/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from translate import Translator
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+
+        text = request.POST['translate']
+        to_lang = request.POST['tolanguage']
+        from_lang = request.POST["fromlanguage"]
+        translator = Translator(to_lang=to_lang, from_lang=from_lang)
+        translation = translator.translate(text)
+        
+        context = {
+            'translation': translation,
+            
+        }
+        return render(request,'index.html', context)
+    return render(request, 'index.html')
